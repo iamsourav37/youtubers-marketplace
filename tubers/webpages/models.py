e0a3f913458a85bcd10afbcefe3e8656b2af8cfd
iamsourav37 <souravganguly108@gmail.com>
2021-10-21 13:32:33 +0530

    Slider is implemented dynamically

--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Slider(models.Model):
+    headline = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=100)
+    button_text = models.CharField(max_length=100)
+    button_url = models.URLField(default='#')
+    photo = models.ImageField(upload_to="media/slider_images/%Y/")
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return self.headline
